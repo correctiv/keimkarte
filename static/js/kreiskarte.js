30fbe83a3c86a7c31c73be025c2a6d4441b22784
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-11-19 05:05:06 +0100

    Escape incoming query parameters to prevent reflected xss

--- static/js/kreiskarte.js
+++ static/js/kreiskarte.js
@@ -21,7 +21,7 @@
     var vars = query.split('&');
     for (var i = 0; i < vars.length; i++) {
       var pair = vars[i].split('=');
-      qobj[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+      qobj[decodeURIComponent(pair[0])] = escape(decodeURIComponent(pair[1]));
     }
     return qobj;
   }
